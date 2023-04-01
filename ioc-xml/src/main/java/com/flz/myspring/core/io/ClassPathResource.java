8c6068c5b33785cb8de09ad333c2e731f64d7871
Fallenz <beijuspsn@163.com>
2023-04-01 18:22:19 +0800

    fix: 如果没有传入类加载器，就获取当前线程的类加载器

--- ioc-xml/src/main/java/com/flz/myspring/core/io/ClassPathResource.java
+++ ioc-xml/src/main/java/com/flz/myspring/core/io/ClassPathResource.java
@@ -1,7 +1,5 @@
 package com.flz.myspring.core.io;
 
-import org.apache.commons.lang3.ClassUtils;
-
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
@@ -17,7 +15,7 @@ public class ClassPathResource implements Resource {
 
     public ClassPathResource(String path, ClassLoader classLoader) {
         this.path = path;
-        this.classLoader = classLoader;
+        this.classLoader = classLoader == null ? Thread.currentThread().getContextClassLoader() : classLoader;
     }
 
     @Override
