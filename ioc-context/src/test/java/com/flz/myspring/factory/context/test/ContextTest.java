4bfc5c0cd1ef1806df8e6e3fb0a28fa817278ab6
Fallenz <beijuspsn@163.com>
2023-05-02 17:16:42 +0800

    feat: 为BPP和BFPP增加新的测试，覆盖无任何PP注册的场景

--- ioc-context/src/test/java/com/flz/myspring/factory/context/test/ContextTest.java
+++ ioc-context/src/test/java/com/flz/myspring/factory/context/test/ContextTest.java
@@ -14,4 +14,13 @@ class ContextTest {
         Assertions.assertEquals("Valve update to Valve-2.0", userService.getCompany());
         Assertions.assertEquals("location update to Chengdu", userService.getLocation());
     }
+
+    @Test
+    void should_create_bean_without_any_bean_post_processors_and_bean_factory_post_processors_executing() {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:myspring-context-without-pp.xml");
+        UserService userService = context.getBean("userService", UserService.class);
+        Assertions.assertEquals("context-3号", userService.queryUserInfo("1003"));
+        Assertions.assertEquals("Valve", userService.getCompany());
+        Assertions.assertEquals("America", userService.getLocation());
+    }
 }
